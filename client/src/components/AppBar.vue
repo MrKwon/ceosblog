85f793121383b2c15d726206a125c780519a7360
mrkwon <erased11@gmail.com>
2019-04-05 14:17:56 +0900

    implement appbar and title

---
+++ client/src/components/AppBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar>
+    <v-container>
+    <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+      <v-toolbar-title>CEOS 기술 블로그</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items> -->
+    </v-container>
+  </v-toolbar>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
